a8a388896350e3e14c07a5edcf32f5d17326c99b
Rajat Bhatt <rajatbhatt999@gmail.com>
2023-10-30 20:46:39 -0400

    Add skills icon

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,31 +1,41 @@
+import {
+  SiGraphql,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiPostgresql,
+  SiReact,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si";
+
 export function Skills() {
   return (
     <section id="skills" className="pt-20">
       <h2 className="text-center text-xl font-bold">Skills</h2>
-      <ul className="mt-4 flex list-inside flex-wrap justify-center gap-2 font-semibold md:mx-auto md:max-w-xs">
-        <li className="rounded-full border-2 border-slate-900 px-4 py-2 dark:border-slate-200">
-          React
+      <ul className="mt-4 flex list-inside flex-wrap justify-center gap-2 font-semibold md:mx-auto md:max-w-2xl">
+        <li className="rounded-lg border-2 border-slate-900 px-4 py-2 dark:border-slate-200">
+          <SiReact className="mr-1 inline-block h-8 w-8" /> React
         </li>
-        <li className="rounded-full border-2 border-slate-900 px-4  py-2 dark:border-slate-200">
-          Typescript
+        <li className="rounded-lg border-2 border-slate-900 px-4  py-2 dark:border-slate-200">
+          <SiTypescript className="mr-1 inline-block h-8 w-8" /> Typescript
         </li>
-        <li className="rounded-full border-2 border-slate-900 px-4 py-2 dark:border-slate-200">
-          NextJS
+        <li className="rounded-lg border-2 border-slate-900 px-4 py-2 dark:border-slate-200">
+          <SiNextdotjs className="mr-1 inline-block h-8 w-8" /> NextJS
         </li>
-        <li className="rounded-full border-2 border-slate-900 px-4 py-2 dark:border-slate-200">
-          NodeJS
+        <li className="rounded-lg border-2 border-slate-900 px-4 py-2 dark:border-slate-200">
+          <SiNodedotjs className="mr-1 inline-block h-8 w-8" /> NodeJS
         </li>
-        <li className="rounded-full border-2 border-slate-900 px-4 py-2 dark:border-slate-200">
-          Tailwind
+        <li className="rounded-lg border-2 border-slate-900 px-4 py-2 dark:border-slate-200">
+          <SiTailwindcss className="mr-1 inline-block h-8 w-8" /> Tailwind
         </li>
-        <li className="rounded-full border-2 border-slate-900 px-4 py-2 dark:border-slate-200">
-          Postgres
+        <li className="rounded-lg border-2 border-slate-900 px-4 py-2 dark:border-slate-200">
+          <SiPostgresql className="mr-1 inline-block h-8 w-8" /> Postgres
         </li>
-        <li className="rounded-full border-2 border-slate-900 px-4 py-2 dark:border-slate-200">
+        <li className="flex items-center rounded-lg border-2 border-slate-900 px-4 py-2 dark:border-slate-200">
           REST
         </li>
-        <li className="rounded-full border-2 border-slate-900 px-4 py-2 dark:border-slate-200">
-          GraphQL
+        <li className="rounded-lg border-2 border-slate-900 px-4 py-2 dark:border-slate-200">
+          <SiGraphql className="mr-1 inline-block h-8 w-8" /> GraphQL
         </li>
       </ul>
     </section>
