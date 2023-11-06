c45629c0687e5f2c691a334587219d49520ebd3c
Rajat Bhatt <rajatbhatt999@gmail.com>
2023-11-05 20:01:06 -0500

    Add new blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
   );
 
   return (
-    <div className="mx-auto max-w-xl py-8">
+    <div className="mx-auto min-h-[calc(100vh-12rem)] max-w-xl py-8">
       <h2 className="mb-8 text-center text-3xl font-bold">
         Read my blog posts
       </h2>
