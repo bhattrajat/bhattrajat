1a9bd12578e12a0637ac849e244e841445361318
Rajat Bhatt <rajatbhatt999@gmail.com>
2023-11-07 22:10:42 -0500

    Add http1.1-vs-http2 blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -21,7 +21,7 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
   const Content = getMDXComponent(post.body.code);
 
   return (
-    <article className="prose prose-slate mx-auto max-w-xl dark:prose-invert prose-code:before:hidden prose-code:after:hidden lg:max-w-2xl">
+    <article className="prose prose-slate mx-auto max-w-xl dark:prose-invert prose-code:before:hidden prose-code:after:hidden lg:max-w-3xl">
       <div className="not-prose mb-8">
         <h1 className="text-2xl font-bold">{post.title}</h1>
         <time dateTime={post.date} className="text-xs dark:text-slate-200">
